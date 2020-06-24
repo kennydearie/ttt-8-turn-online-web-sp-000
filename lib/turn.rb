6cb7dc894c86cee88208287e6f307be9adead278
Nkenge Simms <simmsn@yahoo.com>
2020-06-24 00:08:38 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,55 @@
+board = [" "," "," "," "," "," "," "," "," "]
+       
+
+def display_board(board) 
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "  
+  
+end
+ 
+ def input_to_index(user_input)
+   user_input.to_i - 1 
+ end
+ 
+ def move(board, user_input, player = "X")
+   board[user_input] = player
+ end
+ 
+ def valid_move?(board, index)
+   index.between?(0,8) && !position_taken?(board, index)
+ end
+ 
+ def position_taken?(board, index)
+   board[index] == "X" || board[index] == "O"
+ end
+ 
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  input_to_index(user_input)
+if valid_move?(board,0)
+  return(true) 
+ 
+else position_taken?(board,0)
+  
+   return(display_board)
+  end
+end
+ 
+ 
+ 
+# def turn(board)
+ # puts "Please enter 1-9:"
+  #user_input = gets.strip
+  #index = input_to_index(user_input)
+  #if valid_move?(board, index)
+   # move(board, user_input, player="X")
+    #display_board(board)
+  #else
+   # turn(board)
+  #end
+#end
+ 
